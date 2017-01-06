641c80948b7b2fcbf1b19832fe53c8badc5ead1d
yinguozi <yinguozi169@pingan.com.cn>
2017-01-06 10:28:09 +0800

    add vue and threeJS

---
+++ Vue/test-vue/src/components/Hugh.vue
@@ -0,0 +1,17 @@
+<template>
+    <div  class="hugh" @click="btnClick">{{msg}}</div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      msg: '你好！'
+    }
+  },
+  methods: {
+    btnClick () {
+      console.log('123')
+    }
+  }
+}
+</script>
